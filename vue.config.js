bca28024a51fea0c07e42c497c6aa0e0e3dff9c0
tommytang <tommy.tang@lixinchuxing.com>
2019-09-02 14:55:19 +0800

    fix: 修改publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const pkg = require('./package.json')
 
 const isProduction = process.env.RUN_ENV === 'production';
-const baseUrl = 'https://tomgou.github.io/canvas-example/www';
+const baseUrl = 'www';
 module.exports = {
   baseUrl: isProduction ?  baseUrl: '/',
   outputDir: 'www',
